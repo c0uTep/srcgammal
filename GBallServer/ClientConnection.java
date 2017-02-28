98fb35d2f23c375bc52ef0ad706ff65b2c8dd587
Pothorian <a15gusvi@hs.local>
2017-02-28 10:57:00 +0100

    ClientConnection sendmsg fix

--- GBallServer/ClientConnection.java
+++ GBallServer/ClientConnection.java
@@ -1,10 +1,13 @@
 package GBallServer;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketTimeoutException;
+import Shared.MsgData;
 
 public class ClientConnection {
 	private final InetAddress m_address;
@@ -12,33 +15,29 @@ public class ClientConnection {
 
 	public ClientConnection(InetAddress address, int port) {
 		m_address = address;
-		m_port = port;		
+		m_port = port;
 	}
 	
-	public void handshake()
-	{
-		
-	}
-	
-	public void sendMessage(String message, DatagramSocket socket) {
-		/*byte[] ackBuff = new byte[256];
-		DatagramPacket ackPacket = new DatagramPacket(ackBuff, ackBuff.length);
-		
-		byte[] buffer = new byte[256];
-		buffer = message.getBytes();
-		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, m_address, m_port);
-		
-		try
-		{
-			System.out.println("Sending. Server -> Client");
-			socket.send(packet);
+	public void sendMessage(MsgData data, DatagramSocket socket) {
+		ByteArrayOutputStream bStream = new ByteArrayOutputStream();
+		ObjectOutput output;
+		try {
+			output = new ObjectOutputStream(bStream);
+			output.writeObject(data);
+			output.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
-		
-		catch (IOException e)
-		{
-			// TODO Auto-generated catch block
+
+		byte[] serializedMessage = bStream.toByteArray();
+
+		DatagramPacket packet = new DatagramPacket(serializedMessage, serializedMessage.length, m_address, m_port);
+		try {
+			socket.send(packet);
+		} catch (IOException e) {
+			System.err.println("Failed to send message.");
 			e.printStackTrace();
-		}*/
+		}
 	}
 
 	public InetAddress getAddress() {
