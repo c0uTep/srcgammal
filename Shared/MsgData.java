ad54ed1c81ed970eb28a1268fe23420504ebf7fd
c0uTep <a15johsy@hs.local>
2017-02-28 11:57:46 +0100

    Changed in MSGData
    Removed initial position and initial speed. Added playerID

--- Shared/MsgData.java
+++ Shared/MsgData.java
@@ -10,35 +10,33 @@ public class MsgData implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	public String m_string;
+	public int m_playerID;
 	
+	public Vector2D m_ballPosition;
 	public Vector2D m_position;
-	public Vector2D m_initialPosition;
-	public Vector2D m_initialDirection;
 	public Vector2D m_speed;
 	public Vector2D m_direction; // Should always be unit vector; determines the
 									// object's facing
 
 	public MsgData() {
 
+		m_playerID = -1;
+		m_ballPosition = new Vector2D();
 		m_position = new Vector2D();
-		m_initialPosition = new Vector2D();
-		m_initialDirection = new Vector2D();
 		m_speed = new Vector2D();
 		m_direction = new Vector2D();
 		m_string = null;
 	}
 
-	public MsgData(Vector2D position, Vector2D initialPosition, Vector2D initialDirection, Vector2D speed,
-			Vector2D direction) {
+	public MsgData(int playerID, Vector2D position, Vector2D speed, Vector2D direction) {
 
+		m_playerID = playerID;
 		m_position = position;
-		m_initialPosition = initialPosition;
-		m_initialDirection = initialDirection;
 		m_speed = speed;
 		m_direction = direction;
 	}
 	
-	public MsgData(String input){
+	public MsgData(String input) {
 		m_string = input;
 	}
 
