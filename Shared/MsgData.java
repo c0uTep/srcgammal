3679ed404b9319f87c419c65ebaeb15d6f26d090
c0uTep <a15johsy@hs.local>
2017-03-02 15:50:51 +0100

    Fixed Merge conflict

--- Shared/MsgData.java
+++ Shared/MsgData.java
@@ -10,34 +10,20 @@ public class MsgData implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	public String m_string;
-<<<<<<< HEAD
-	public int m_playerID;
-	
-	public Vector2D m_ballPosition;
-	public Vector2D m_position;
-=======
 	public EntitiesHolder m_entities;
 	public int m_playerID = -1;
 	
 	/*public Vector2D m_position;
 	public Vector2D m_initialPosition;
 	public Vector2D m_initialDirection;
->>>>>>> origin/master
 	public Vector2D m_speed;
 	public Vector2D m_direction; // Should always be unit vector; determines the
 									// object's facing*/
 
 	public MsgData() {
-
-<<<<<<< HEAD
-		m_playerID = -1;
-		m_ballPosition = new Vector2D();
-		m_position = new Vector2D();
-=======
 		/*m_position = new Vector2D();
 		m_initialPosition = new Vector2D();
 		m_initialDirection = new Vector2D();
->>>>>>> origin/master
 		m_speed = new Vector2D();
 		m_direction = new Vector2D();*/
 		
@@ -45,12 +31,8 @@ public class MsgData implements Serializable {
 		m_entities = null;
 	}
 
-<<<<<<< HEAD
-	public MsgData(int playerID, Vector2D position, Vector2D speed, Vector2D direction) {
-=======
 	/*public MsgData(Vector2D position, Vector2D initialPosition, Vector2D initialDirection, Vector2D speed,
 			Vector2D direction) {
->>>>>>> origin/master
 
 		m_playerID = playerID;
 		m_position = position;
@@ -67,11 +49,7 @@ public class MsgData implements Serializable {
 		m_string = score;
 	}
 	
-<<<<<<< HEAD
-	public MsgData(String input) {
-=======
 	public MsgData(String input, int playerIdentification){
->>>>>>> origin/master
 		m_string = input;
 		m_playerID = playerIdentification;
 	}
